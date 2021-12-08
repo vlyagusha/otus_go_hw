4084b4bd799fd90b958c746b03cd23cccf802657
Vyacheslav Lyagusha <vlyagusha@gmail.com>
2021-12-08 20:50:05 +0300

    hw3 is done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,79 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+	"unicode"
+)
+
+func Top10(text string) []string {
+	wordsMap := getWordsCountMap(text)
+
+	return getTopSortedWords(wordsMap, 10)
+}
+
+func getTopSortedWords(words map[string]int, top int) []string {
+	type WordCounter struct {
+		Word  string
+		Count int
+	}
+	type WordCounterSlice []WordCounter
+
+	wordCounterList := make(WordCounterSlice, len(words))
+	i := 0
+	for k, v := range words {
+		wordCounterList[i] = WordCounter{k, v}
+		i++
+	}
+
+	sort.Slice(wordCounterList, func(i, j int) bool {
+		if wordCounterList[i].Count == wordCounterList[j].Count {
+			return wordCounterList[i].Word < wordCounterList[j].Word
+		}
+		return wordCounterList[i].Count > wordCounterList[j].Count
+	})
+
+	var result []string
+	for i := 0; i < len(wordCounterList); i++ {
+		if i >= top {
+			break
+		}
+		result = append(result, wordCounterList[i].Word)
+	}
+
+	return result
+}
+
+func getWordsCountMap(text string) map[string]int {
+	words := make(map[string]int, 0)
+	runes := []rune(text)
+	i, s := 0, ""
+	for {
+		i, s = getWordFromPosition(i, runes)
+		if i >= len(runes) {
+			break
+		}
+		if _, ok := words[s]; ok {
+			words[s] += 1
+		} else {
+			words[s] = 1
+		}
+	}
+
+	return words
+}
+
+func getWordFromPosition(pos int, runes []rune) (int, string) {
+	if pos >= len(runes) {
+		return pos, ""
+	}
+
+	res := strings.Builder{}
+	for ; pos < len(runes) && unicode.IsSpace(runes[pos]); pos++ {
+	}
+	for ; pos < len(runes) && !unicode.IsSpace(runes[pos]); pos++ {
+		res.WriteRune(runes[pos])
+	}
+
+	return pos, res.String()
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -48,6 +48,10 @@ func TestTop10(t *testing.T) {
 		require.Len(t, Top10(""), 0)
 	})
 
+	t.Run("only spaces", func(t *testing.T) {
+		require.Len(t, Top10("                         "), 0)
+	})
+
 	t.Run("positive test", func(t *testing.T) {
 		if taskWithAsteriskIsCompleted {
 			expected := []string{
@@ -78,5 +82,51 @@ func TestTop10(t *testing.T) {
 			}
 			require.Equal(t, expected, Top10(text))
 		}
+	})
+
+	t.Run("all words are equal", func(t *testing.T) {
+		expected := []string{
+			"test",
+		}
+		require.Equal(t, expected, Top10(`
+			test test test test test test test test test test test test 
+			test test test test test test test test test test test test 
+			test test test test test test test test test test test test 
+			test test test test test test test test test test test test 
+			test test test test test test test test test test test test 
+			test test test test test test test test test test test test 
+		`))
+	})
+
+	t.Run("top-15 words with equal count", func(t *testing.T) {
+		expected := []string{
+			"a",
+			"b",
+			"c",
+			"d",
+			"e",
+			"f",
+			"g",
+			"h",
+			"i",
+			"j",
+		}
+		require.Equal(t, expected, Top10(`
+			a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a 
+			b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b 
+			c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 
+			d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d 
+			e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e 
+			f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+			g g g g g g g g g g g g g g g g g g g g g g g g g g g g g g g g g 
+			h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h 
+			i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i 
+			j j j j j j j j j j j j j j j j j j j j j j j j j j j j j j j j j 
+			k k k k k k k k k k k k k k k k k k k k k k k k k k k k k k k k k 
+			l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l 
+			m m m m m m m m m m m m m m m m m m m m m m m m m m m m m m m m m 
+			o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o 
+			p p p p p p p p p p p p p p p p p p p p p p p p p p p p p p p p p 
+		`))
 	})
 }
