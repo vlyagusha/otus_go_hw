149991cead7a8fc63b3449d80ee034739f1eb867
Vyacheslav Lyagusha <vlyagusha@gmail.com>
2022-01-12 19:55:48 +0300

    hw5 run with all errors completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,7 +2,9 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -11,11 +13,17 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
+	var errorsCount = int32(m)
+
 	tasksCh := make(chan Task)
 	wg := sync.WaitGroup{}
 
 	go func() {
 		for _, task := range tasks {
+			if atomic.LoadInt32(&errorsCount) <= 0 {
+				close(tasksCh)
+				return
+			}
 			tasksCh <- task
 		}
 		close(tasksCh)
@@ -26,12 +34,24 @@ func Run(tasks []Task, n, m int) error {
 		go func() {
 			defer wg.Done()
 			for task := range tasksCh {
-				task()
+				if atomic.LoadInt32(&errorsCount) <= 0 {
+					return
+				}
+				err := task()
+				if err != nil {
+					atomic.AddInt32(&errorsCount, -1)
+				}
 			}
 		}()
 	}
 
 	wg.Wait()
+
+	fmt.Println(atomic.LoadInt32(&errorsCount))
+
+	if atomic.LoadInt32(&errorsCount) <= 0 {
+		return ErrErrorsLimitExceeded
+	}
 
 	return nil
 }
