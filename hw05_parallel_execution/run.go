3bcbb0c132f1856d7e5adbda99a4e58d7275caad
Vyacheslav Lyagusha <vlyagusha@gmail.com>
2022-01-24 22:05:16 +0300

    hw5 fixes after review

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -20,24 +20,11 @@ func Run(tasks []Task, n, m int) error {
 	tasksCh := make(chan Task)
 	wg := sync.WaitGroup{}
 
-	go func() {
-		defer close(tasksCh)
-		for _, task := range tasks {
-			if atomic.LoadInt32(&errorsCount) <= 0 {
-				return
-			}
-			tasksCh <- task
-		}
-	}()
-
 	for i := 0; i < n; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			for task := range tasksCh {
-				if atomic.LoadInt32(&errorsCount) <= 0 {
-					return
-				}
 				err := task()
 				if err != nil {
 					atomic.AddInt32(&errorsCount, -1)
@@ -45,6 +32,14 @@ func Run(tasks []Task, n, m int) error {
 			}
 		}()
 	}
+
+	for _, task := range tasks {
+		if atomic.LoadInt32(&errorsCount) <= 0 {
+			break
+		}
+		tasksCh <- task
+	}
+	close(tasksCh)
 
 	wg.Wait()
 
