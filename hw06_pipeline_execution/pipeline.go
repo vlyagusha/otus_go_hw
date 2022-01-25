b7667118d75f3e72c0d5be624d027ca33a477a87
Vyacheslav Lyagusha <vlyagusha@gmail.com>
2022-01-25 22:52:52 +0300

    hw6 done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,15 +9,32 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	out := make(Bi)
-	defer close(out)
-
-	switch len(stages) {
-	case 0:
-		return out
-	case 1:
-		return stages[0](in)
-	default:
-		return ExecutePipeline(stages[0](in), done, stages[1:]...)
+	out := in
+	for _, stage := range stages {
+		out = doneStage(done, out)
+		out = stage(out)
 	}
+
+	return out
+}
+
+func doneStage(done In, in In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- v
+			}
+		}
+	}()
+
+	return out
 }
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -91,6 +91,31 @@ func TestPipeline(t *testing.T) {
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
 
+	t.Run("done right now case", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		close(done)
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, done, stages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Len(t, result, 0)
+		require.Less(t, int64(elapsed), int64(fault))
+	})
+
 	t.Run("zero stages", func(t *testing.T) {
 		in := make(Bi)
 		data := []int{1, 2, 3, 4, 5}
@@ -102,17 +127,15 @@ func TestPipeline(t *testing.T) {
 			close(in)
 		}()
 
-		result := make([]string, 0, 10)
+		result := make([]int, 0, 10)
 		start := time.Now()
 		for s := range ExecutePipeline(in, nil, []Stage{}...) {
-			result = append(result, s.(string))
+			result = append(result, s.(int))
 		}
 		elapsed := time.Since(start)
 
-		require.Equal(t, []string{}, result)
-		require.Less(t,
-			int64(elapsed),
-			int64(sleepPerStage)+int64(fault))
+		require.Equal(t, []int{1, 2, 3, 4, 5}, result)
+		require.Less(t, int64(elapsed), int64(sleepPerStage)+int64(fault))
 	})
 
 	t.Run("only one stage", func(t *testing.T) {
@@ -136,8 +159,6 @@ func TestPipeline(t *testing.T) {
 		elapsed := time.Since(start)
 
 		require.Equal(t, []string{"1", "2", "3", "4", "5"}, result)
-		require.Less(t,
-			int64(elapsed),
-			int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
+		require.Less(t, int64(elapsed), int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
 	})
 }
