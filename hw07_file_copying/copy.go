2c84595e81178832ae5d486a2d6b3fa22077a58e
Vyacheslav Lyagusha <vlyagusha@gmail.com>
2022-01-30 23:33:18 +0300

    hw7 done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +12,60 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	var fileRead, fileWrite *os.File
+	var err error
+
+	fileRead, err = os.OpenFile(fromPath, os.O_RDONLY, 0o644)
+	if err != nil {
+		return err
+	}
+
+	fileWrite, err = os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, 1024)
+	_, err = fileRead.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	for {
+		read, err := fileRead.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if limit > 0 && int64(read) >= limit {
+			_, err := fileWrite.Write(buf[:limit])
+			if err != nil {
+				return err
+			}
+			break
+		}
+		limit -= int64(read)
+
+		_, err = fileWrite.Write(buf[:read])
+		if err != nil {
+			return err
+		}
+	}
+
+	defer func() error {
+		err = fileRead.Close()
+		if err != nil {
+			return err
+		}
+
+		err = fileWrite.Close()
+		if err != nil {
+			return err
+		}
+		return nil
+	}()
+
 	return nil
 }
