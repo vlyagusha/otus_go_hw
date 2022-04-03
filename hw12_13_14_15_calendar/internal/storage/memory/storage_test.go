7610d07f128c2212a8414405621a91399511c33a
Vyacheslav Lyagusha <vlyagusha@gmail.com>
2022-04-03 23:36:44 +0300

    hw14 tests added

--- hw12_13_14_15_calendar/internal/storage/memory/storage_test.go
+++ hw12_13_14_15_calendar/internal/storage/memory/storage_test.go
@@ -95,4 +95,65 @@ func TestStorage(t *testing.T) {
 		}
 		require.Len(t, saved, 0)
 	})
+
+	t.Run("test ", func(t *testing.T) {
+		events := []memorystorage.Event{
+			{
+				ID:        parseUUID("4927aa58-a175-429a-a125-c04765597150"),
+				StartedAt: parseDateTime("2022-04-03 11:59:59"),
+				Notify:    parseDateTime("2022-04-03 11:59:59"),
+			},
+			{
+				ID:        parseUUID("4927aa58-a175-429a-a125-c04765597151"),
+				StartedAt: parseDateTime("2022-04-03 12:00:00"),
+				Notify:    parseDateTime("2022-04-03 12:00:00"),
+			},
+			{
+				ID:        parseUUID("4927aa58-a175-429a-a125-c04765597152"),
+				StartedAt: parseDateTime("2022-04-04 12:00:00"),
+				Notify:    parseDateTime("2022-04-03 12:00:00"),
+			},
+			{
+				ID:        parseUUID("4927aa58-a175-429a-a125-c04765597153"),
+				StartedAt: parseDateTime("2022-04-05 12:00:01"),
+				Notify:    parseDateTime("2022-04-04 11:59:01"),
+			},
+		}
+
+		for _, e := range events {
+			_ = storage.Create(e)
+		}
+
+		readyEvents, err := storage.GetEventsReadyToNotify(parseDateTime("2022-04-03 12:00:00"))
+		require.Nil(t, err)
+
+		ids := extractEventIDs(readyEvents)
+		idsExpected := []string{
+			"4927aa58-a175-429a-a125-c04765597150",
+			"4927aa58-a175-429a-a125-c04765597151",
+			"4927aa58-a175-429a-a125-c04765597152",
+		}
+		require.Equal(t, idsExpected, ids)
+	})
+}
+
+func parseUUID(stringUUID string) uuid.UUID {
+	res, _ := uuid.Parse(stringUUID)
+
+	return res
+}
+
+func parseDateTime(stringDateTime string) time.Time {
+	res, _ := time.Parse("2006-01-02 15:04:05", stringDateTime)
+
+	return res
+}
+
+func extractEventIDs(events []memorystorage.Event) []string {
+	res := make([]string, 0, len(events))
+	for _, e := range events {
+		res = append(res, e.ID.String())
+	}
+
+	return res
 }
